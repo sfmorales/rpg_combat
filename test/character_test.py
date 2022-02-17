0a50e7adb034df7314c4d82a4eea8cef73e9f2b6
Chris Caragianis <chris_caragianis@mednax.com>
2022-02-17 09:54:32 -0500

    test for smack

--- test/character_test.py
+++ test/character_test.py
@@ -6,3 +6,9 @@ def test_character_starts_alive():
     assert character.health == 1000
     assert character.level == 1
     assert character.alive
+
+def test_character_can_damage_character():
+    rogue = Character()
+    barbarian = Character()
+    barbarian.smack(rogue) 
+    assert rogue.health < 1000
