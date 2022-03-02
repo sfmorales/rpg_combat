2d9cce77f2822d99efbd4de9fa76126c5e0fc65c
Kyle Adams <kadams54@users.noreply.github.com>
2022-03-02 09:15:37 -0500

    Don't allow healing past max health

--- rpg_combat/character.py
+++ rpg_combat/character.py
@@ -1,18 +1,25 @@
+MAX_HEALTH = 1000
+
+
 class Character(object):
     def __init__(self):
-        self.health = 1000
+        self.health = MAX_HEALTH
         self.level = 1
         self.alive = True
 
     def smack(self, character):
         character.health = character.health - 10
-        character.checkhealth()
+        character.normalize_status()
 
-    def checkhealth(self):
+    def heal(self, character):
+        if not character.alive:
+            return
+        character.health = character.health + 10
+        character.normalize_status()
+
+    def normalize_status(self):
         if self.health <= 0:
             self.health = 0
             self.alive = False
-
-    def heal(self, character):
-        if character.alive:
-            character.health = character.health + 10
+        if self.health > MAX_HEALTH:
+            self.health = MAX_HEALTH
