501c0bbb0048795b3bc4b42bb49f0cff5a48e30c
Chris Caragianis <chris_caragianis@mednax.com>
2022-03-02 10:16:36 -0500

    health starts at 1000, failing test for initial level

---
+++ rpg_combat/character.py
@@ -0,0 +1,3 @@
+class Character:
+    def __init__(self):
+        self.health = 1000
--- test/character_test.py
+++ test/character_test.py
@@ -1,5 +1,11 @@
 from rpg_combat.character import Character
 
+
 def test_character_health_starts_at_1000():
-  character = Character()
-  assert character.health == 1000
+    character = Character()
+    assert character.health == 1000
+
+
+def test_character_level_starts_at_1():
+    character = Character()
+    assert character.level == 1
