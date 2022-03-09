74dd0f65755f5df48a9bb9ac4ab913025473f596
smorales <shauna_flowersmorales@mednax.com>
2022-03-09 09:54:59 -0500

    Added class for character and test for level

---
+++ rpg_combat/character.py
@@ -0,0 +1,5 @@
+class Character:
+  def __init__(self) -> None:
+      self.health = 1000
+      print("Setting Health")
+      print (self.health)
--- test/character_test.py
+++ test/character_test.py
@@ -4,3 +4,7 @@ from rpg_combat.character import Character
 def test_character_starts_with_1000_health():
     wizard = Character()
     assert wizard.health == 1000
+
+def test_character_level_starts_at_1():
+    wizard = Character()
+    assert wizard.level == 1
